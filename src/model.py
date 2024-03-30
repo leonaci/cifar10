765a59c88ea8328a48842ba856171a7298e01bba
leonaci <leonacism@gmail.com>
2024-03-30 14:04:52 +0900

    層を追加

--- src/model.py
+++ src/model.py
@@ -13,18 +13,21 @@ class ImageClassifier(nn.Module):
             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
-        
+
         self.classifier = nn.Sequential(
-            nn.Linear(64 * 4 * 4, 512),
+            nn.Linear(128 * 2 * 2, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 256),
             nn.ReLU(inplace=True),
