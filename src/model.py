622732f0959ebfc21a0fe674de250cf00c0943f7
leonaci <leonacism@gmail.com>
2024-03-30 15:17:20 +0900

    層を追加

--- src/model.py
+++ src/model.py
@@ -9,29 +9,42 @@ class ImageClassifier(nn.Module):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(f"Using device: {torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'CPU'}")
         
+        ## (1, 3, 32, 32) -> (1, 64, 16, 16)
         self.layer1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
+        )
+
+        ## (1, 64, 16, 16) -> (1, 256, 8, 8)
+        self.layer2 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
         )
 
+        ## (1, 256, 8, 8) -> (1, 128, 4, 4)
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        ## (1, 128 * 4 * 4) -> (1, 10)
         self.classifier = nn.Sequential(
-            nn.Linear(128 * 2 * 2, 512),
+            nn.Linear(128 * 4 * 4, 512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 256),
             nn.ReLU(inplace=True),
@@ -42,6 +55,8 @@ class ImageClassifier(nn.Module):
 
     def forward(self, x):
         x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
