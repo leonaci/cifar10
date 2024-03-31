dcff5eeecdcd59c2789e87f9507e8d9d4363cb85
leonaci <leonacism@gmail.com>
2024-03-31 11:23:20 +0900

    ResNetブロックに置き換え

--- src/model.py
+++ src/model.py
@@ -1,20 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-## (batch_size, channels, w, h) -> (batch_size, channels, w, h)
-def conv(channels, kernel_size=3):
-    if kernel_size % 2 == 0:
-        raise ValueError("Kernel size must be odd!")
-
-    return nn.Sequential(
-        nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1),
-        nn.BatchNorm2d(channels),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1),
-        nn.BatchNorm2d(channels),
-        nn.ReLU(inplace=True)
-    )
+from resnet import ResNetBlock
 
 class ImageClassifier(nn.Module):
     def __init__(self):
@@ -26,51 +13,20 @@ class ImageClassifier(nn.Module):
         ## (batch_size, channels, w, h) -> (batch_size, channels, w // 2, h // 2)
         self.maxpool = nn.MaxPool2d(kernel_size=2)
 
-        ## (batch_size, 3, 32, 32) -> (batch_size, 64, 32, 32)
-        self.input_layer = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-        )
-
-        ## (batch_size, 64, 16, 16) -> (batch_size, 128, 8, 8)
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-        )
-
-        ## (batch_size, 128, 8, 8) -> (batch_size, 256, 4, 4)
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-        )
-
-        ## (batch_size, 256, 4, 4) -> (batch_size, 512, 2, 2)
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-        )
-
-        ## (batch_size, 512, w, h) -> (batch_size, 512, 1, 1)
+        ## (batch_size, channels, w, h) -> (batch_size, channels, 1, 1)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        ## (batch_size, 3, 32, 32) -> (batch_size, 64, 32, 32)
+        self.input_layer = ResNetBlock(in_channels=3, out_channels=64, short=False)
+
+        ## (batch_size, 64, 16, 16) -> (batch_size, 128, 16, 16)
+        self.layer1 = ResNetBlock(in_channels=64, out_channels=128, short=False)
+
+        ## (batch_size, 128, 8, 8) -> (batch_size, 256, 8, 8)
+        self.layer2 = ResNetBlock(in_channels=128, out_channels=256, short=False)
+
+        ## (batch_size, 256, 4, 4) -> (batch_size, 512, 4, 4)
+        self.layer3 = ResNetBlock(in_channels=256, out_channels=512, short=False)
 
         ## (batch_size, 512) -> (batch_size, 10)
         self.classifier = nn.Linear(512, 10)
@@ -80,10 +36,18 @@ class ImageClassifier(nn.Module):
     def forward(self, x):
         x = self.input_layer(x)
         x = self.maxpool(x)
+
         x = self.layer1(x)
+        x = self.maxpool(x)
+
         x = self.layer2(x)
+        x = self.maxpool(x)
+
         x = self.layer3(x)
+        x = self.maxpool(x)
+
         x = self.avg_pool(x)
         x = x.flatten(1)
+
         x = self.classifier(x)
         return x
---
+++ src/resnet.py
@@ -0,0 +1,68 @@
+from typing import Any, Callable, List, Optional, Type, Union
+
+import torch
+from torch import Tensor 
+import torch.nn as nn
+import torch.nn.functional as F
+
+## (batch_size, in_channels, w, h) -> (batch_size, out_channels, (w - 1) // stride + 1, (h - 1) // stride + 1) 
+def conv(in_channels: int, out_channels: int, kernel_size: int, stride: int = 1):
+    if kernel_size % 2 == 0:
+        raise ValueError("Kernel size must be odd!")
+    return nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        padding=kernel_size // 2,
+        stride=stride,
+        bias=False
+    )
+
+def conv1x1(in_channels: int, out_channels: int, stride: int = 1):
+    return conv(in_channels, out_channels, kernel_size=1, stride=stride)
+
+def conv3x3(in_channels: int, out_channels: int, stride: int = 1):
+    return conv(in_channels, out_channels, kernel_size=3, stride=stride)
+
+def conv5x5(in_channels: int, out_channels: int, stride: int = 1):
+    return conv(in_channels, out_channels, kernel_size=5, stride=stride)
+
+def conv7x7(in_channels: int, out_channels: int, stride: int = 1):
+    return conv(in_channels, out_channels, kernel_size=7, stride=stride)
+
+## (batch_size, in_channels, w, h) -> (batch_size, out_channels, (w - 1) // stride + 1, (h - 1) // stride + 1)
+class ResNetBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, short: bool = True):
+        super().__init__()
+
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        if in_channels == out_channels and stride == 1:
+            self.shortcut = nn.Identity()
+        else:
+            self.shortcut = nn.Sequential(
+                conv1x1(in_channels, out_channels, stride),
+                nn.BatchNorm2d(out_channels)
+            )
+
+        self.short = short
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.short:
+            out += self.shortcut(x)
+
+        out = self.relu(out)
+
+        return out
