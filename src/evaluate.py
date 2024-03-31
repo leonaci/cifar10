9f15a312f735da966b242bb959998447cef643bf
leonaci <leonacism@gmail.com>
2024-04-01 00:27:15 +0900

    出力図のレイアウトを変更

--- src/evaluate.py
+++ src/evaluate.py
@@ -99,23 +99,24 @@ class Evaluator:
         fig, ax1 = plt.subplots(figsize=(8, 6))
         ax2 = ax1.twinx()
 
-        ax1.plot(self.train_loss_history, linewidth=2, color='#FDB813', label='Train Loss')
-        ax1.plot(self.valid_loss_history, linewidth=2, color='#B2D732', label='Valid Loss')
+        train_loss_line = ax1.plot(self.train_loss_history, linewidth=2, color='#FDB813', label='Train Loss')
+        valid_loss_line = ax1.plot(self.valid_loss_history, linewidth=2, color='#B2D732', label='Valid Loss')
         ax1.set_xlabel('Epochs')
         ax1.set_ylabel('Loss')
-        ax1.set_yscale('log')
         ax1.set_xlim(1, self.num_epochs)
-        ax1.set_ylim(1e-3, 1)
+        ax1.set_ylim(ymax=1)
         ax1.grid(True)
-        ax1.legend(loc='upper left')
 
-        ax2.plot(self.train_acc_history, linewidth=1, color='#FD7F20', label='Train Accuracy')
-        ax2.plot(self.valid_acc_history, linewidth=1, color='#87CB16', label='Valid Accuracy')
+        train_acc_line = ax2.plot(self.train_acc_history, linewidth=1, linestyle="dashed", color='#FD7F20', label='Train Accuracy')
+        valid_acc_line = ax2.plot(self.valid_acc_history, linewidth=1, linestyle="dashed", color='#87CB16', label='Valid Accuracy')
         ax2.set_ylabel('Accuracy')
         ax2.set_xlim(1, self.num_epochs)
         ax2.set_ylim(0, 100)
-        ax2.legend(loc='upper right')
+
+        lines = train_loss_line + valid_loss_line + train_acc_line + valid_acc_line
+        labels = map(lambda line: line.get_label(), lines)
+        plt.legend(lines, labels, loc='lower left')
 
         plt.title('Loss and Accuracy')
-        plt.savefig('../data/loss_accuracy_graph.png')
+        plt.savefig('../data/loss_and_accuracy.png')
         plt.close()
